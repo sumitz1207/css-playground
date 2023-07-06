e4648b54ab8830cff2b61d6f0232de70e5fff334
Sumit Patel <sumitz1207@gmail.com>
2023-07-05 23:16:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,16 @@ class MyHeader extends React.Component {
    
 }
 }
+class MyFooter extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Hello Style!</h1>
+      <p>Add a little style!.</p>
+      </div>
+    );
+   
+}
+}
 
 ReactDOM.render(<MyHeader />, document.getElementById('root'));
